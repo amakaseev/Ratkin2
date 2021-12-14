2115bf5a580003d5f853fd69faafa23aea359783
hazed <aleksey.makaseev@gmail.com>
2021-12-14 14:47:44 +0200

    fix camera rotation

--- Assets/Scripts/EditorCamera.cs
+++ Assets/Scripts/EditorCamera.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EditorCamera: MonoBehaviour {
 
-  public Transform  vertical;
+  public Transform  yaw;
+  public Transform  pitch;
+
   public float      moveSpeed;
   public float      lookSpeed;
 
   Vector2 _moveDirection = Vector3.zero;
   bool    _lookActive;
+  Vector2 _zoom;
 
   void Start() {
     Application.targetFrameRate = 60;
@@ -28,19 +32,28 @@ public class EditorCamera: MonoBehaviour {
   public void OnLookDelta(InputValue input) {
     Vector2 lookDirection = input.Get<Vector2>();
     if (_lookActive) {
-      vertical.transform.Rotate(lookDirection.y * lookSpeed, 0.0f, 0.0f);
-      transform.Rotate(0.0f, lookDirection.x * lookSpeed, 0.0f);
+      yaw.transform.Rotate(0f, lookDirection.x * lookSpeed, 0f);
+      pitch.transform.Rotate(lookDirection.y * lookSpeed, 0f, 0f);
     }
   }
 
+  public void OnZoom(InputValue input) {
+    _zoom = input.Get<Vector2>();
+  }
+
   public void Update() {
+    var dt = Time.deltaTime;
+
     if (_moveDirection != Vector2.zero) {
-      var dt = Time.deltaTime;
-      var right = new Vector3(1, 0, 0) * _moveDirection.x * moveSpeed * dt;
-      var forward = new Vector3(0, 0, 1) * _moveDirection.y * moveSpeed * dt;
+      var right = yaw.right * _moveDirection.x * moveSpeed * dt;
+      var forward = yaw.forward * _moveDirection.y * moveSpeed * dt;
 
       transform.Translate(forward);
       transform.Translate(right);
+    }
+
+    if (_zoom != Vector2.zero) {
+      transform.Translate(pitch.forward * _zoom.y * moveSpeed * dt);
     }
   }
 
