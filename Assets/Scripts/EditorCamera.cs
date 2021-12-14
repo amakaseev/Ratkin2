b10a5f1d1840f656f7ff015741012e3c1fd1ad8e
hazed <aleksey.makaseev@gmail.com>
2021-12-14 13:32:09 +0200

    acc camera control in editor

--- Assets/Scripts/EditorCamera.cs
+++ Assets/Scripts/EditorCamera.cs
@@ -10,35 +10,37 @@ public class EditorCamera: MonoBehaviour {
   public float      lookSpeed;
 
   Vector2 _moveDirection = Vector3.zero;
-  Vector2 _lookDirection = Vector3.zero;
   bool    _lookActive;
+
+  void Start() {
+    Application.targetFrameRate = 60;
+    QualitySettings.vSyncCount = 1;
+  }
 
   public void OnMove(InputValue input) {
     _moveDirection = input.Get<Vector2>();
-    Debug.Log("OnMove");
   }
 
   public void OnLookActive(InputValue input) {
     _lookActive = (input.Get<float>() == 0f)? false : true;
-    Debug.Log("lookActive: " + _lookActive);
   }
 
   public void OnLookDelta(InputValue input) {
-    _lookDirection = input.Get<Vector2>();
-    Debug.Log("OnLook: " + _lookDirection);
+    Vector2 lookDirection = input.Get<Vector2>();
+    if (_lookActive) {
+      vertical.transform.Rotate(lookDirection.y * lookSpeed, 0.0f, 0.0f);
+      transform.Rotate(0.0f, lookDirection.x * lookSpeed, 0.0f);
+    }
   }
 
   public void Update() {
     if (_moveDirection != Vector2.zero) {
       var dt = Time.deltaTime;
-      var position = transform.position;
-      position.x += _moveDirection.x * moveSpeed * dt;
-      position.z += _moveDirection.y * moveSpeed * dt;
-      transform.position = position;
-    }
-    if (_lookActive) {
-      var dt = Time.deltaTime;
-      transform.Rotate(_lookDirection.y * lookSpeed * dt, 0.0f, 0.0f);
+      var right = new Vector3(1, 0, 0) * _moveDirection.x * moveSpeed * dt;
+      var forward = new Vector3(0, 0, 1) * _moveDirection.y * moveSpeed * dt;
+
+      transform.Translate(forward);
+      transform.Translate(right);
     }
   }
 
