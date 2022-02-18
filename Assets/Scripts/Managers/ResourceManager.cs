711cd2e313a6b7e2139dc6227fcc7be69dc7a558
hazed <aleksey.makaseev@gmail.com>
2022-02-18 12:49:11 +0200

    update resource

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+// TODO: make as singleton
 public class ResourceManager : MonoBehaviour {
 
 	public static Dictionary<string, GameObject> platforms { get; } = new Dictionary<string, GameObject>();
@@ -11,13 +12,12 @@ public class ResourceManager : MonoBehaviour {
 		LoadDefaultResources();
 	}
 
-	private async void LoadDefaultResources() {
+	private static async void LoadDefaultResources() {
 		var loadHandle = Addressables.LoadAssetsAsync<GameObject>("platforms",
 		                                                          addressable => {
 			                                                          platforms.Add(addressable.name, addressable);
-			                                                          Debug.Log(addressable);
+			                                                          Debug.Log("Loaded: " + addressable);
 		                                                          });
-		
 		await loadHandle.Task;
 		Actions.OnLoadCopmlete();
 	}
